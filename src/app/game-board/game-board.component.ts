04f2189190613bf8786a32a94023b725ba798798
jfaria62 <jfaria62@gmail.com>
2020-06-18 22:42:44 -0400

    started spot submission function

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PokeAPIService } from './poke-api.service';
+import { PokeAPIService } from '../poke-api.service';
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
 export class GameBoardComponent implements OnInit {
+  PokeService: PokeAPIService;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectBox(side){
+    this.PokeService.getPokemon(side);
+  }
 }
