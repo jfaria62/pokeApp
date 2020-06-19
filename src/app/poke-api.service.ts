097b7440696f037b6560cebae3a2497c1eb21081
jfaria62 <jfaria62@gmail.com>
2020-06-18 22:16:49 -0400

    updated and changed pokeapi calls for data

--- src/app/poke-api.service.ts
+++ src/app/poke-api.service.ts
@@ -1,32 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Pokemon} from './pokemon';
-
+import {Pokemon} from './model/pokemon';
+// import {map} from 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/toPromise';
-import {map} from 'rxjs/operators';
+import {map, catchError} from 'rxjs/operators';
 // import {  } from '@angular/common/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
-@Injectable({
-  providedIn: 'root'
-})
-
+@Injectable()
 export class PokeAPIService {
   // base urls to access PokeAPI stuff
   private baseUrl = 'https://pokeapi.co/api/v2/';
   private baseSpriteUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
   // HttpClient request in PokeAPIService constructor
   constructor(private http: HttpClient) { }
 
-  getSprite() {
-    return this.http.get<string>(this.baseSpriteUrl + '4.png');
-  }
-  // GET pokemon information offset: number, limit: number
+  // GET pokemon information offset
   getPokemon() {
-    return this.http.get<string>( this.baseUrl + 'pokemon/charmander');
+    const obj = this.http.get(this.baseUrl + 'pokedex/1/').pipe(map(pkm => pkm.toString()));
+    return obj;
+    /*.toPromise()
+      .then((res: any) => {
+        let pokemons: Pokemon[] = [];
+        let reducePokeEntries = JSON.parse(res._body).pokemon_entries.splice(0, 50);
+
+        reducePokeEntries.forEach(element => {
+          let pokemon = new Pokemon();
+          pokemon.name = element.pokemon_species.name;
+          pokemon.id = element.entry_number;
+
+          pokemons.push(pokemon);
+        });
+        console.log('Pokemons: ' + pokemons);
+        return pokemons;
+      });
+      */
+    // return this.http.get( this.baseUrl + 'pokemon/' + pokename).pipe(
+    //  map(this.extractData));
+
 
     /*.toPromise()
     .then(items => items.isPrototypeOf((item, idx) => {
@@ -39,4 +54,12 @@ export class PokeAPIService {
       };
     }));*/
   }
+
+  getSprite(pokeID: string) {
+    return this.http.get<string>(this.baseSpriteUrl + '.png');
+  }
+
+  /*getEvolutions() {
+    return this.http.get(this.baseUrl + )
+  }*/
 }
