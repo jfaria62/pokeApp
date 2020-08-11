7522a5a523043365adba22b98bb480e48009c007
jfaria62 <jfaria62@gmail.com>
2020-08-11 16:58:49 -0400

    added functions for getting chosen pkmn number

--- src/app/poke-api.service.ts
+++ src/app/poke-api.service.ts
@@ -1,63 +1,78 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Pokemon} from './model/pokemon';
-// import {map} from 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/toPromise';
-import {map, catchError} from 'rxjs/operators';
-// import {  } from '@angular/common/';
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Subject, Observable } from 'rxjs';
+import { LogService } from './log.service';
+import { Pokemon } from './model/pokemon';
 
 @Injectable()
 export class PokeAPIService {
   // base urls to access PokeAPI stuff
   private baseUrl = 'https://pokeapi.co/api/v2/';
   private baseSpriteUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+  private pokeList = [];
+
+  private logService: LogService;
+  pokeSubject = new Subject<void>();
+  httpClient: HttpClient;
+  private playerOne: number;
+  private playerTwo: number;
+  sendData$: Observable<any>;
+
 
   // HttpClient request in PokeAPIService constructor
-  constructor(private http: HttpClient) { }
-
-  // GET pokemon information offset
-  getPokemon() {
-    const obj = this.http.get(this.baseUrl + 'pokedex/1/').pipe(map(pkm => pkm.toString()));
-    return obj;
-    /*.toPromise()
-      .then((res: any) => {
-        let pokemons: Pokemon[] = [];
-        let reducePokeEntries = JSON.parse(res._body).pokemon_entries.splice(0, 50);
-
-        reducePokeEntries.forEach(element => {
-          let pokemon = new Pokemon();
-          pokemon.name = element.pokemon_species.name;
-          pokemon.id = element.entry_number;
-
-          pokemons.push(pokemon);
-        });
-        console.log('Pokemons: ' + pokemons);
-        return pokemons;
-      });
-      */
-    // return this.http.get( this.baseUrl + 'pokemon/' + pokename).pipe(
-    //  map(this.extractData));
-
-
-    /*.toPromise()
-    .then(items => items.isPrototypeOf((item, idx) => {
-      const id: number = idx + offset + 1;
-
-      return{
-        name: item.name,
-        sprite: '${this.baseSpriteUrl}${id}.png',
-        id
-      };
-    }));*/
+  constructor(logService: LogService, private http: HttpClient) {
+    this.logService = logService;
+    this.http = http;
+    this.sendData$ = this.pokeSubject.asObservable();
   }
 
+  getNumberOne() {
+    console.log(this.playerOne);
+    return this.playerOne;
+  }
+
+
+  getNumberTwo() {
+    return this.playerTwo;
+  }
+
+  setNumber(name, player) {
+    // find the position of the character chosen in pokeList
+    const pos = this.pokeList.findIndex((char) => {
+      return char.name === name;
+    });
+    // find which player chose the character
+    if (player === 1) {
+      this.playerOne = pos + 1;
+    } else {
+      this.playerTwo = pos + 1;
+    }
+    // return data to subscribers
+    this.pokeSubject.next();
+  }
+
+  // Call API for data and set pokeList
+  fetchPokemon() {
+    // returns observable object
+    this.http.get<Pokemon>(this.baseUrl + 'pokemon?limit=151').subscribe(
+      (data) => {
+        const extractedChars = data.results;
+        const chars = extractedChars.map((char) => {
+          return { name: char.name, url: char.url };
+        });
+
+        this.pokeList = chars;
+        this.pokeSubject.next();
+      });
+  }
+
+  getPokemon() {
+      return this.pokeList;
+  }
+  /*
   getSprite(pokeID: string) {
     return this.http.get<string>(this.baseSpriteUrl + '.png');
-  }
+  }*/
 
   /*getEvolutions() {
     return this.http.get(this.baseUrl + )
