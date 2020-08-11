324901450e0352a1e5a9440c4e675d837ee7fbdb
jfaria62 <jfaria62@gmail.com>
2020-08-11 16:59:59 -0400

    added routing and subscription

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { PokeAPIService } from '../poke-api.service';
+import { ActivatedRoute } from '@angular/router';
+
+
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
 export class GameBoardComponent implements OnInit {
-  PokeService: PokeAPIService;
+  pokeList = [];
+  pokeService: PokeAPIService;
+  activatedRoute: ActivatedRoute;
+  subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(activatedRoute: ActivatedRoute, pokeService: PokeAPIService) {
+    this.activatedRoute = activatedRoute;
+    this.pokeService = pokeService;
   }
 
-  selectBox(side){
-    this.PokeService.getPokemon();
+  ngOnInit() {
+    const pic = document.getElementById('A1');
+    //pic.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3'  + '.png');
+
+    // subscription to call getPokemon method for name list data
+    this.subscription = this.pokeService.pokeSubject.subscribe(
+      (params) => {
+        this.pokeList = this.pokeService.getPokemon();
+      }
+    );
+
+    this.activatedRoute.params.subscribe(
+      () => {
+        this.pokeList = this.pokeService.getPokemon();
+      }
+    );
+  }
+
+  selectBox(side) {
+    this.pokeService.getPokemon();
   }
 }
