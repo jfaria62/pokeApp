e6040cd5e6f385786f26179862eee1100480f2d2
jfaria62 <jfaria62@gmail.com>
2020-08-11 17:14:16 -0400

    added functions to change player icon

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,24 +1,71 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import {PokeAPIService} from '../poke-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
+
 export class TabsComponent implements OnInit {
-  @Input() labelKey = 'label';
-  @Input() idKey = 'id';
-  @Input() pokemon = [];
+  pokeList = [];
+  pokeService: PokeAPIService;
+  activatedRoute: ActivatedRoute;
+  subscription;
+  iconOne;
+  iconTwo = document.getElementById('player2');
 
-  constructor() { }
-
-  ngOnInit() {
-    let x;
-    let pokeName = [];
-    for ( x = 1; x <= 150; x++) {
-      this.pokemon[x] = x;
-      //pokeName[x] =
-    }
+  constructor(activatedRoute: ActivatedRoute, pokeService: PokeAPIService) {
+    this.activatedRoute = activatedRoute;
+    this.pokeService = pokeService;
   }
 
+  chooseIcon(name, player) {
+    this.pokeService.setNumber(name, player);
+    console.log(name);
+    this.fetchIcons();
+  }
+
+  fetchIcons() {
+    let p1 = this.pokeService.getNumberOne();
+    let p2 = this.pokeService.getNumberTwo();
+    if (p1) {
+      this.iconOne.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'  + p1 + '.png');
+    }
+    if (p2) {
+      this.iconTwo.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'  + p2 + '.png');
+    }
+
+  }
+
+  ngOnInit() {
+    this.iconOne = document.getElementById('player1');
+    this.iconTwo = document.getElementById('player2');
+    // call fetchPokemon method to API
+    this.pokeService.fetchPokemon();
+    // this.iconOne.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + this.iconOne + '.png');
+    // subscription to call getPokemon method for name list data
+    this.subscription = this.pokeService.pokeSubject.subscribe(
+      (params) => {
+        this.pokeList = this.pokeService.getPokemon();
+      }
+    );
+
+    this.activatedRoute.params.subscribe(
+      () => {
+        this.pokeList = this.pokeService.getPokemon();
+      }
+    );
+
+      /*
+
+
+      const pokeList = this.pokeService.getPokemon();
+     console.log( pokeList );
+     for ( x = 1; x <= 150; x++) {
+      this.pokemon[x] = x;
+      //pokeName[x] =
+    }*/
+  }
 }
